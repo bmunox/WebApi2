2d65f92e06f053ed5ac1d50ca81f8062db6bf579
Bryan Munoz <brimunvi@hotmail.com>
2021-04-20 18:58:56 -0600

    WebApi2

---
+++ WebApi2/Controllers/PersonaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Add(Models.Request.requestData model)
+        {
+            model.Message = "Exito";
+            return Ok(model.Message);
+        }
+    }
+}
---
+++ WebApi2/Models/Request/requestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi2.Models.Request
+{
+    public class requestData
+    {
+        public string json_data { get; set; }
+        public string ResponseCode { get; set; }
+        public string Message { get; set; }
+    }
+}
